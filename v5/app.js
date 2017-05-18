89eceac672e9dec16f96bc6d1bff028904afe17b
soniravi829 <soniravi829@gmail.com>
2017-05-18 12:01:53 -0400

    add login functionality

--- v5/app.js
+++ v5/app.js
@@ -161,6 +161,18 @@ app.post("/register", function(req, res){
   });
 });
 
+// Add login routes
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+// Login logic
+app.post("/login", passport.authenticate("local", {
+  successRedirect: "/campgrounds",
+  failureRedirect: "/login"
+}), function(req, res){
+  
+});
 
 
 //=====================================================================
